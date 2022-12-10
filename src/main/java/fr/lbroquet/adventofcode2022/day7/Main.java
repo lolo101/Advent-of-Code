c36096b8a840217c1ecdc7b039fd295c13ac7517
lolo101 <lbroquet@online.fr>
2022-12-10 17:45:46 +0100

    size of the smallest directory to delete to recover enough space for the update

--- src/main/java/fr/lbroquet/adventofcode2022/day7/Main.java
+++ src/main/java/fr/lbroquet/adventofcode2022/day7/Main.java
@@ -3,18 +3,24 @@ package fr.lbroquet.adventofcode2022.day7;
 import fr.lbroquet.Input;
 
 public class Main {
+    private static final int DISK_SPACE = 70_000_000;
+    private static final int REQUIRED_SPACE = 30_000_000;
+
     public static void main(String[] args) {
         DirectoryHierarchy hierarchy = Input.load(Main.class).lines()
                 .collect(
                         DirectoryHierarchy::new,
                         DirectoryHierarchy::run,
                         DirectoryHierarchy::merge);
-        int sum = hierarchy.root.scanDirectoriesDepthFirst()
+        int sizeToReclaim = REQUIRED_SPACE - DISK_SPACE + hierarchy.root.size();
+        int sizeOfDirectoryToDelete = hierarchy.root.scanDirectoriesDepthFirst()
                 .mapToInt(Directory::size)
-                .filter(size -> size <= 100_000)
-                .sum();
+                .filter(size -> size >= sizeToReclaim)
+                .min()
+                .orElseThrow();
 
         System.out.printf("Total size: %d%n", hierarchy.root.size());
-        System.out.printf("Sum of size of directories of size lte 100 000: %d%n", sum);
+        System.out.printf("Size to recover: %d%n", sizeToReclaim);
+        System.out.printf("Size of smallest directory to delete: %d%n", sizeOfDirectoryToDelete);
     }
 }
