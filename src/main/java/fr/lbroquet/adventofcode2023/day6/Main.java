2d5b248327d7bb61be27025aa9555899f91d85e3
lolo101 <lbroquet@online.fr>
2023-12-07 00:16:10 +0100

    advent of code 2023 day 6 part 2

--- src/main/java/fr/lbroquet/adventofcode2023/day6/Main.java
+++ src/main/java/fr/lbroquet/adventofcode2023/day6/Main.java
@@ -12,6 +12,11 @@ public class Main {
     private static final Pattern SPACES = Pattern.compile("\\s+");
 
     public static void main(String[] args) throws IOException {
+        part1();
+        part2();
+    }
+
+    private static void part1() throws IOException {
         try (BufferedReader input = Input.load(Main.class)) {
             String[] times = SPACES.split(input.readLine().substring("Time:".length()).trim());
             String[] distances = SPACES.split(input.readLine().substring("Distance:".length()).trim());
@@ -22,6 +27,17 @@ public class Main {
             }
             long waysToBeatAllRaces = races.stream().mapToLong(Race::waysToBeat).reduce(1, Math::multiplyExact);
             System.out.printf("Ways to beat all races: %d%n", waysToBeatAllRaces);
+        }
+    }
+
+    private static void part2() throws IOException {
+        try (BufferedReader input = Input.load(Main.class)) {
+            String time = SPACES.matcher(input.readLine().substring("Time:".length()).trim()).replaceAll("");
+            String distance = SPACES.matcher(input.readLine().substring("Distance:".length()).trim()).replaceAll("");
+
+            Race race = new Race(time, distance);
+            long waysToBeatRace = race.waysToBeat();
+            System.out.printf("Ways to beat long race: %d%n", waysToBeatRace);
         }
     }
 }
