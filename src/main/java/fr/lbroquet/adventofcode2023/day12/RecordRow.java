2bc071679bf5020d552927b8bcde80d762c23a83
lolo101 <lbroquet@online.fr>
2023-12-13 23:28:16 +0100

    advent of code 2023 day 12 part 1 wip

--- src/main/java/fr/lbroquet/adventofcode2023/day12/RecordRow.java
+++ src/main/java/fr/lbroquet/adventofcode2023/day12/RecordRow.java
@@ -2,7 +2,8 @@ package fr.lbroquet.adventofcode2023.day12;
 
 import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.SequencedCollection;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class RecordRow {
     private final String conditions;
@@ -14,26 +15,51 @@ public class RecordRow {
     }
 
     public long arrangements() {
-        SequencedCollection<String> chunks = new LinkedList<>(Arrays.asList(conditions.split("\\.")));
-        SequencedCollection<String> groups = new LinkedList<>(Arrays.asList(contiguousGroups));
+        List<String> chunks = new LinkedList<>(Arrays.asList(conditions.split("\\.")));
+        List<Integer> groups = new LinkedList<>(Stream.of(contiguousGroups).mapToInt(Integer::parseInt).boxed().toList());
 
-        long arrangements = 1;
-        for (String group : groups) {
-            arrangements *= arrangement(chunks.removeFirst(), Long.parseLong(group));
-        }
-        return arrangements;
+        return arrangements(chunks, groups);
     }
 
-    private static long arrangement(String chunk, long contiguous) {
+    private static long arrangements(List<String> chunks, List<Integer> contiguousGroups) {
+        if (chunks.isEmpty()) {
+            return contiguousGroups.isEmpty() ? 1 : 0;
+        }
+        if (contiguousGroups.isEmpty()) {
+            return chunks.stream().noneMatch(chunk -> chunk.contains("#")) ? 1 : 0;
+        }
+
+        String chunk = chunks.getFirst();
+        int contiguous = contiguousGroups.getFirst();
+
         if (chunk.length() < contiguous) {
-            return 0;
+            if (chunk.contains("#")) return 0;
+            return arrangements(chunks, tail(contiguousGroups));
         }
         if (chunk.length() == contiguous) {
-            return 1;
+            return arrangements(tail(chunks), tail(contiguousGroups));
         }
         if (chunk.startsWith("?")) {
-            return 1 + arrangement(chunk.substring(1), contiguous);
+            return arrangements(replaceHead(chunk.substring(1), tail(chunks)), contiguousGroups)
+                    + matchContiguousAndContinue(chunk, contiguous, tail(chunks), tail(contiguousGroups));
         }
-        return 1;
+        return matchContiguousAndContinue(chunk, contiguous, tail(chunks), tail(contiguousGroups));
+    }
+
+    private static long matchContiguousAndContinue(String chunk, int contiguous, List<String> chunksTail, List<Integer> groupsTail) {
+        return chunk.charAt(contiguous) == '?'
+                ? arrangements(replaceHead(chunk.substring(1 + contiguous), chunksTail), groupsTail)
+                : 0;
+    }
+
+    private static <T> List<T> tail(List<T> list) {
+        return list.subList(1, list.size());
+    }
+
+    private static <T> List<T> replaceHead(T head, List<T> tail) {
+        List<T> list = new LinkedList<>();
+        list.add(head);
+        list.addAll(tail);
+        return list;
     }
 }
--- src/test/java/fr/lbroquet/adventofcode2023/day12/RecordRowTest.java
+++ src/test/java/fr/lbroquet/adventofcode2023/day12/RecordRowTest.java
@@ -7,6 +7,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class RecordRowTest {
 
     @Test
+    void zero_chunk_zero_group_one_arrangements() {
+        RecordRow recordRow = new RecordRow("", new String[0]);
+        assertEquals(1, recordRow.arrangements());
+    }
+
+    @Test
+    void only_unknown_chunk_zero_group_one_arrangements() {
+        RecordRow recordRow = new RecordRow("???", new String[0]);
+        assertEquals(1, recordRow.arrangements());
+    }
+
+    @Test
     void one_chunk_one_group_two_arrangements() {
         RecordRow recordRow = new RecordRow("???", new String[]{"2"});
         assertEquals(2, recordRow.arrangements());
@@ -16,6 +28,12 @@ class RecordRowTest {
     void one_chunk_with_damaged_one_group_one_arrangements() {
         RecordRow recordRow = new RecordRow("#??", new String[]{"2"});
         assertEquals(1, recordRow.arrangements());
+    }
+
+    @Test
+    void one_chunk_with_damaged_one_group_zero_arrangements() {
+        RecordRow recordRow = new RecordRow("?#?#", new String[]{"2"});
+        assertEquals(0, recordRow.arrangements());
     }
 
     @Test
