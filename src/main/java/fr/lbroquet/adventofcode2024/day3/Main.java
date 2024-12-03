439a098922c10ca042f03a9ad4522e03171dd495
lolo101 <lbroquet@online.fr>
2024-12-03 21:55:15 +0100

    advent of code 2024 day 3 part 2

--- src/main/java/fr/lbroquet/adventofcode2024/day3/Main.java
+++ src/main/java/fr/lbroquet/adventofcode2024/day3/Main.java
@@ -7,7 +7,8 @@ import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class Main {
-    private static final Pattern MUL = Pattern.compile("mul\\((?<firstOperand>\\d{1,3}),(?<secondOperand>\\d{1,3})\\)");
+    private static final Pattern MUL = Pattern.compile("mul\\((?<firstOperand>\\d{1,3}),(?<secondOperand>\\d{1,3})\\)|(?<do>do\\(\\))|(?<dont>don't\\(\\))");
+    private static boolean enabled = true;
 
     public static void main(String[] args) {
         long sum = Input.load(Main.class).lines().flatMap(Main::toExpression).mapToLong(Main::multiply).sum();
@@ -19,8 +20,21 @@ public class Main {
     }
 
     private static long multiply(MatchResult matchResult) {
-        String firstOperand = matchResult.group("firstOperand");
-        String secondOperand = matchResult.group("secondOperand");
-        return Long.parseLong(firstOperand) * Long.parseLong(secondOperand);
+        String enable = matchResult.group("do");
+        String disable = matchResult.group("dont");
+        if (enable != null) {
+            enabled = true;
+            return 0;
+        }
+        if (disable != null) {
+            enabled = false;
+            return 0;
+        }
+        if (enabled) {
+            String firstOperand = matchResult.group("firstOperand");
+            String secondOperand = matchResult.group("secondOperand");
+            return Long.parseLong(firstOperand) * Long.parseLong(secondOperand);
+        }
+        return 0;
     }
 }
