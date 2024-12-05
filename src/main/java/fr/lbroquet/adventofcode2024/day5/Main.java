79cce6f9d8f2099a9155727dc366dbe2554a3226
lolo101 <lbroquet@online.fr>
2024-12-06 00:34:59 +0100

    advent of code 2024 day 5 part 2

--- src/main/java/fr/lbroquet/adventofcode2024/day5/Main.java
+++ src/main/java/fr/lbroquet/adventofcode2024/day5/Main.java
@@ -5,7 +5,9 @@ import fr.lbroquet.Input;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -22,12 +24,25 @@ public class Main {
         }
 
         PageComparator pageComparator = new PageComparator(rules);
-        int sum = updates
+        int sumOrdered = updates
                 .stream()
                 .filter(pageComparator::ordered)
                 .mapToInt(Main::middlePage)
                 .sum();
-        System.out.println("Sum of middle pages: " + sum);
+        System.out.println("Sum of ordered middle pages: " + sumOrdered);
+
+        int sumInitiallyUnordered = updates
+                .stream()
+                .filter(update -> !pageComparator.ordered(update))
+                .map(update -> sort(update, pageComparator))
+                .mapToInt(Main::middlePage)
+                .sum();
+        System.out.println("Sum of unordered middle pages after ordering: " + sumInitiallyUnordered);
+    }
+
+    private static String[] sort(String[] update, Comparator<String> pageComparator) {
+        Arrays.sort(update, pageComparator);
+        return update;
     }
 
     private static int middlePage(String[] update) {
