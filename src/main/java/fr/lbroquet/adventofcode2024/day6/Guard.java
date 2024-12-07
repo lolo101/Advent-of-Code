ac48b957e91e99a88be06fa2106caf1e40614df9
lolo101 <lbroquet@online.fr>
2024-12-08 00:15:24 +0100

    advent of code 2024 day 6 part 2 WIP

--- src/main/java/fr/lbroquet/adventofcode2024/day6/Guard.java
+++ src/main/java/fr/lbroquet/adventofcode2024/day6/Guard.java
@@ -1,28 +1,15 @@
 package fr.lbroquet.adventofcode2024.day6;
 
-public final class Guard {
-    private Position position;
-    private Facing facing;
-
-    public Guard(Position position, Facing facing) {
-        this.position = position;
-        this.facing = facing;
-    }
-
+public record Guard(Position position, Facing facing) {
     public Position forwardPosition() {
-        return switch (facing) {
-            case NORTH -> new Position(position.row() - 1, position.column());
-            case EAST -> new Position(position.row(), position.column() + 1);
-            case SOUTH -> new Position(position.row() + 1, position.column());
-            case WEST -> new Position(position.row(), position.column() - 1);
-        };
+        return facing.forward(position);
     }
 
-    public void stepForward() {
-        position = forwardPosition();
+    public Guard stepForward() {
+        return new Guard(forwardPosition(), facing);
     }
 
-    public void turnRight() {
-        facing = facing.turnRight();
+    public Guard turnRight() {
+        return new Guard(position, facing.turnRight());
     }
 }
--- src/main/java/fr/lbroquet/adventofcode2024/day6/SituationMap.java
+++ src/main/java/fr/lbroquet/adventofcode2024/day6/SituationMap.java
@@ -2,46 +2,98 @@ package fr.lbroquet.adventofcode2024.day6;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.SortedMap;
-import java.util.TreeMap;
 
 public class SituationMap {
-    private final SortedMap<Position, Tile> tiles = new TreeMap<>();
+    private final char[][] map;
     private final Set<Position> visited = new HashSet<>();
+    private final Set<Guard> visitedObstacles = new HashSet<>();
+    private final Set<Position> possibleObstructions = new HashSet<>();
 
     public SituationMap(char[][] map) {
+        this.map = map;
         for (int row = 0; row < map.length; row++) {
             for (int column = 0; column < map[row].length; column++) {
                 Position position = new Position(row, column);
-                Tile tile = new Tile(position, map[row][column]);
-                tiles.put(position, tile);
-                if (tile.startingPosition()) {
+                if (map[row][column] == '^') {
                     visited.add(position);
                 }
             }
         }
+        Guard guard = new Guard(visited.stream().findFirst().orElseThrow(), Facing.NORTH);
+        walk(guard);
     }
 
-    public long visitedPositions() {
-        walk();
+    public String print() {
+        StringBuilder output = new StringBuilder();
+        for (int row = 0; row < map.length; row++) {
+            for (int column = 0; column < map[row].length; column++) {
+                if (possibleObstructions.contains(new Position(row, column))) {
+                    output.append("\u001b[93mO\u001b[0m");
+                } else if (visited.contains(new Position(row, column))) {
+                    output.append("\u001b[31mx\u001b[0m");
+                } else {
+                    output.append(map[row][column]);
+                }
+            }
+            output.append('\n');
+        }
+        return output.toString();
+    }
+
+    public int visitedPositions() {
         return visited.size();
     }
 
-    private void walk() {
-        Position currentPosition = visited.stream().findFirst().orElseThrow();
-        Guard guard = new Guard(currentPosition, Facing.NORTH);
+    public int possibleObstructions() {
+        return possibleObstructions.size();
+    }
+
+    private void walk(Guard guard) {
         for (
                 Position forwardPosition = guard.forwardPosition();
-                tiles.containsKey(forwardPosition);
+                insideMap(forwardPosition);
                 forwardPosition = guard.forwardPosition()
         ) {
-            Tile forwardTile = tiles.get(forwardPosition);
-            if (forwardTile.isBlocked()) {
-                guard.turnRight();
+            if (blocked(forwardPosition)) {
+                visitedObstacles.add(guard);
+                guard = guard.turnRight();
             } else {
                 visited.add(forwardPosition);
-                guard.stepForward();
+                if (sendGhostWalkInLoop(guard.turnRight())) {
+                    possibleObstructions.add(forwardPosition);
+                }
+                guard = guard.stepForward();
             }
         }
+    }
+
+    private boolean sendGhostWalkInLoop(Guard guard) {
+        Set<Guard> ghostVisitedObstacle = new HashSet<>(visitedObstacles);
+        for (
+                Position forwardPosition = guard.forwardPosition();
+                insideMap(forwardPosition);
+                forwardPosition = guard.forwardPosition()
+        ) {
+            if (blocked(forwardPosition)) {
+                boolean alreadyVisited = !ghostVisitedObstacle.add(guard);
+                if (alreadyVisited) {
+                    return true;
+                }
+                guard = guard.turnRight();
+            } else {
+                guard = guard.stepForward();
+            }
+        }
+        return false;
+    }
+
+    private boolean insideMap(Position position) {
+        int row = position.row();
+        int column = position.column();
+        return 0 <= row && row < map.length && 0 <= column && column < map[row].length;
+    }
+
+    private boolean blocked(Position position) {
+        return map[position.row()][position.column()] == '#';
     }
 }
