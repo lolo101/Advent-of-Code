67cc2559f5418774992236010fbebdc5224cbf9d
lolo101 <lbroquet@online.fr>
2025-12-09 20:13:46 +0100

    refactor to use gatherer

--- src/main/java/fr/lbroquet/adventofcode2025/day6/Main.java
+++ src/main/java/fr/lbroquet/adventofcode2025/day6/Main.java
@@ -3,33 +3,34 @@ package fr.lbroquet.adventofcode2025.day6;
 import fr.lbroquet.Input;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Arrays;
+import java.util.stream.Gatherer;
 
 public class Main {
     static void main() throws IOException {
         char[][] mathSheet = transpose(Input.loadMap(Main.class));
 
-        Collection<Operation> operations = new ArrayList<>();
-        Collection<Long> operands = new ArrayList<>();
-        char operator = '\0';
-        for (char[] lineChars : mathSheet) {
-            char maybeOperator = lineChars[lineChars.length - 1];
-            if (maybeOperator == '+' || maybeOperator == '*') {
-                operator = maybeOperator;
-            }
-            String lineString = new String(lineChars, 0, lineChars.length - 1).trim();
-            if(lineString.isBlank()) {
-                operations.add(new Operation(operator, operands.toArray(new Long[0])));
-                operands.clear();
-            } else {
-                operands.add(Long.parseLong(lineString));
-            }
-        }
-        // add last pending operation
-        operations.add(new Operation(operator, operands.toArray(new Long[0])));
+        long total = Arrays.stream(mathSheet)
+                .<Operation>gather(Gatherer.ofSequential(
+                        Memo::new,
+                        (memo, lineChars, downstream) -> {
+                            char maybeOperator = lineChars[lineChars.length - 1];
+                            if (maybeOperator == '+' || maybeOperator == '*') {
+                                memo.operator = maybeOperator;
+                            }
+                            String lineString = new String(lineChars, 0, lineChars.length - 1).trim();
+                            if (lineString.isBlank()) {
+                                downstream.push(new Operation(memo.operator, memo.operands.toArray(new Long[0])));
+                                memo.operands.clear();
+                            } else {
+                                memo.operands.add(Long.parseLong(lineString));
+                            }
+                            return true;
+                        },
+                        (memo, downstream) -> downstream.push(new Operation(memo.operator, memo.operands.toArray(new Long[0])))
+                ))
+                .reduce(0L, (sum, op) -> sum + op.compute(), Long::sum);
 
-        long total = operations.stream().reduce(0L, (sum, op) -> sum + op.compute(), Long::sum);
         IO.println("Grand total=" + total);
     }
 
---
+++ src/main/java/fr/lbroquet/adventofcode2025/day6/Memo.java
@@ -0,0 +1,9 @@
+package fr.lbroquet.adventofcode2025.day6;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+class Memo {
+    public char operator;
+    public final Collection<Long> operands = new ArrayList<>();
+}
